16201484de6a293310324710bd4098f556f89a96
JDFDuarte <joanaiduartes@gmail.com>
2023-09-28 19:00:08 +0200

    Initial import

---
+++ cra_forecast.py
@@ -0,0 +1,10 @@
+import os
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+from urllib.request import urlopen
+from PIL import Image
+
+
+# ________________________________________________________________________________________
